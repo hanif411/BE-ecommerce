e14c557d879ee5d727f540c1f36ed316159764e3
hanif <hanifsholihin884@gmail.com>
2025-06-25 05:21:34 +0700

    update index.js  ubah express mongo sanitize

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ import orderRouter from './routes/orderRouter.js';
 import { notFound,errorHandler } from './middlewares/errorMiddleware.js';
 import cookieParser from 'cookie-parser'
 import helmet from 'helmet';
-import ExpressMongoSanitize from 'express-mongo-sanitize';
 dotenv.config();
 
 const app = express()
@@ -17,7 +16,6 @@ const port = 3000
 //Middleware
 app.use(express.json());
 app.use(helmet())
-app.use(ExpressMongoSanitize())
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('./public'));
