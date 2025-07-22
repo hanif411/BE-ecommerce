8447677dfa1ca54446bb6239505b0eda82e6c213
hanif <hanifsholihin884@gmail.com>
2025-07-22 07:47:03 +0700

    updata bug checkout

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -30,7 +30,7 @@ export const createOrder = asyncHandler(async (req, res) => {
     }
     const { nameproduct, priceproduct, _id, stokproduct } = productdata;
 
-    if (cartItem.productquantity > stokproduct) {
+    if (cartItem.quantity > stokproduct) {
       res.status(404);
       throw new Error("jumlah product melebihi dari stok");
     }
