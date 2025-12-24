64b5024d931a0bf41eb738ff34124dba8985815c
hanif <hanifsholihin884@gmail.com>
2025-12-24 10:32:12 +0700

    secure cooke option false

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,80 +2,82 @@ import User from "../models/authModels.js";
 import jwt from "jsonwebtoken";
 import { asyncHandler } from "../middlewares/asyncHandler.js";
 
-const signToken = id =>{
-    return jwt.sign({id}, process.env.JWT_SECRET, {
-        expiresIn: '6d'
-    })
-}
+const signToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "6d",
+  });
+};
 
-const createSendResToken = (user, statuscode, res)=>{
-    const token = signToken(user._id);
+const createSendResToken = (user, statuscode, res) => {
+  const token = signToken(user._id);
 
-    const isDev = process.env.NODE_ENV === 'development' ? false : true;
+  const isDev = process.env.NODE_ENV === "development" ? false : true;
 
-    const cookieOption = {
-        expires : new Date(Date.now() + 6 * 24 * 60 * 60 * 1000),
-        httpOnly : true,
-        secure : isDev
-    }
+  const cookieOption = {
+    expires: new Date(Date.now() + 6 * 24 * 60 * 60 * 1000),
+    httpOnly: true,
+    secure: false,
+  };
 
-    res.cookie('jwt', token, cookieOption);
+  res.cookie("jwt", token, cookieOption);
 
-    user.password = undefined;
+  user.password = undefined;
 
-    res.status(statuscode).json({
-        data : user
-    })
- }
+  res.status(statuscode).json({
+    data: user,
+  });
+};
 
- export const registerUser = asyncHandler(async (req,res) =>{
-    const createUser = await User.create({
-        name : req.body.name,
-        email : req.body.email,
-        password : req.body.password,
-        role : req.body.role
-    })
-    createSendResToken(createUser, 201, res)
- })
+export const registerUser = asyncHandler(async (req, res) => {
+  const createUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    role: req.body.role,
+  });
+  createSendResToken(createUser, 201, res);
+});
 
- export const loginUser = asyncHandler(async (req,res) =>{
-    // buat validasi harus ada email dan pasword
-    if(!req.body.email || !req.body.password){
-        res.status(400);
-        throw new Error  ('email dan password harus di isi')
-    }
+export const loginUser = asyncHandler(async (req, res) => {
+  // buat validasi harus ada email dan pasword
+  if (!req.body.email || !req.body.password) {
+    res.status(400);
+    throw new Error("email dan password harus di isi");
+  }
 
-    //validasi email ada dan sesuai dengan database
-    const user = await User.findOne({email : req.body.email}).select('+password'); 
+  //validasi email ada dan sesuai dengan database
+  const user = await User.findOne({ email: req.body.email }).select(
+    "+password"
+  );
 
-    // check passwordnya
-    if(user && (await user.comparePassword(req.body.password))){
-        createSendResToken(user, 200, res)
-    }else{
-        res.status(400)
-        throw new Error('email atau password salah')
-    }
- })
+  // check passwordnya
+  if (user && (await user.comparePassword(req.body.password))) {
+    createSendResToken(user, 200, res);
+  } else {
+    res.status(400);
+    throw new Error("email atau password salah");
+  }
+});
 
- export const getUser = asyncHandler(async (req,res) =>{
-    const user = await User.findById(req.User._id);
-    if(!user){
-        res.status(404);
-        throw new Error('user tidak ditemukan')
-    }else {
-        res.status(200).json({
-            data : user
-        })
-    }
-    })
-
- export const logoutUser = asyncHandler(async (req,res) =>{
-    res.cookie('jwt',"",{
-        httpOnly : true,
-        expires : new Date(Date.now()),
-    })
+export const getUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.User._id);
+  if (!user) {
+    res.status(404);
+    throw new Error("user tidak ditemukan");
+  } else {
     res.status(200).json({
-        status : 'success',
-        message : 'logout berhasil'
-        })
- })
+      data: user,
+    });
+  }
+});
+
+export const logoutUser = asyncHandler(async (req, res) => {
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(Date.now()),
+  });
+  res.status(200).json({
+    status: "success",
+    message: "logout berhasil",
+  });
+});
