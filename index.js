6b18761cac33238e70ce2f02735bf53f7a0e7815
hanif <hanifsholihin884@gmail.com>
2025-12-27 13:21:14 +0700

    update cors]

--- index.js
+++ index.js
@@ -22,29 +22,29 @@ const port = 3000;
 
 //Middleware
 const allowedOrigins = [
-  "http://localhost:5173",
   "http://localhost:3000",
+  "http://localhost:5173",
   "http://localhost:3001",
   "https://fe-ecommerce-xxx.vercel.app",
 ];
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      // allow server-to-server / postman
-      if (!origin) return callback(null, true);
 
-      if (allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
-app.options("*", cors());
+const corsOptions = {
+  origin: function (origin, callback) {
+    // allow server-to-server / postman
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    }
+    return callback(null, false);
+  },
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 
 app.use(express.json());
 app.use(helmet());
