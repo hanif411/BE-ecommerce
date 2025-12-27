0aeffb22beddb3426b5a998095c6bed1cffeba22
hanif <hanifsholihin884@gmail.com>
2025-12-27 13:43:25 +0700

    update url cors

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:5173",
   "http://localhost:3001",
-  "https://fe-ecommerce-xxx.vercel.app",
+  "https://ecommerce-food-sigma.vercel.app/",
 ];
 
 const corsOptions = {
